2c2836f769e7f2ea4456058777a05842cce6ec30
Yair Even Or <vsync.design@gmail.com>
2020-08-10 15:07:48 +0300

    complete refactor to vanilla js

--- jquery.hoverCarousel.js
+++ jquery.hoverCarousel.js
@@ -1,71 +1,114 @@
-;(function($){
-    "use strict";
+// Hover-Carousel component
+// By Yair Even-Or
+// https://github.com/yairEO/hover-carousel
 
-    var bindToClass      = 'carousel',
-        containerWidth   = 0,
-        scrollWidth      = 0,
-        posFromLeft      = 0,    // Stripe position from the left of the screen
-        stripePos        = 0,    // When relative mouse position inside the thumbs stripe
-        animated         = null,
-        el, $el, ratio, scrollPos, nextMore, prevMore, pos, padding;
+function HoverCarousel( elm, settings ){
+  this.DOM = {
+    scope: elm,
+    wrap: elm.querySelector('ul').parentNode
+  }
+  
+  this.containerWidth = 0;
+  this.scrollWidth = 0;
+  this.posFromLeft = 0;    // Stripe position from the left of the screen
+  this.stripePos = 0;    // When relative mouse position inside the thumbs stripe
+  this.animated = null;
+  this.callbacks = {}
+  
+  this.init()
+}
 
-    // calculate the thumbs container width
-    function calc(e){
-        $el = $(this).find(' > .wrap');
-        el  = $el[0];
+HoverCarousel.prototype = {
+  init(){
+    this.bind()
+  },
+  
+  destroy(){
+    this.DOM.scope.removeEventListener('mouseenter', this.callbacks.onMouseEnter)
+    this.DOM.scope.removeEventListener('mousemove', this.callbacks.onMouseMove)
+  },
 
-        nextMore = prevMore  = false; // reset
+  bind(){
+    this.callbacks.onMouseEnter = this.onMouseEnter.bind(this)
+    this.callbacks.onMouseMove = e => {
+      if( this.mouseMoveRAF ) 
+        cancelAnimationFrame(this.mouseMoveRAF)
 
-        containerWidth       = el.clientWidth;
-        scrollWidth          = el.scrollWidth; // the "<ul>"" width
-        padding              = 0.2 * containerWidth; // padding in percentage of the area which the mouse movement affects
-        posFromLeft          = $el.offset().left;
-        stripePos            = e.pageX - padding - posFromLeft;
-        pos                  = stripePos / (containerWidth - padding*2);
-        scrollPos            = (scrollWidth - containerWidth ) * pos;
-
-        $el.animate({scrollLeft:scrollPos}, 200, 'swing');
-
-        clearTimeout(animated);
-        animated = setTimeout(function(){
-            animated = null;
-        }, 200);
-
-        return this;
+      this.mouseMoveRAF = requestAnimationFrame(this.onMouseMove.bind(this, e))
     }
+    
+    this.DOM.scope.addEventListener('mouseenter', this.callbacks.onMouseEnter)
+    this.DOM.scope.addEventListener('mousemove', this.callbacks.onMouseMove)
+  },
+  
+  // calculate the thumbs container width
+  onMouseEnter(e){
+    this.nextMore = this.prevMore = false // reset
 
-    // move the stripe left or right according to mouse position
-    function move(e){
-        // don't move anything until inital movement on 'mouseenter' has finished
-        if( animated ) return;
+    this.containerWidth       = this.DOM.wrap.clientWidth;
+    this.scrollWidth          = this.DOM.wrap.scrollWidth; 
+    // padding in percentage of the area which the mouse movement affects
+    this.padding              = 0.2 * this.containerWidth; 
+    this.posFromLeft          = this.DOM.wrap.getBoundingClientRect().left;
+    var stripePos             = e.pageX - this.padding - this.posFromLeft;
+    this.pos                  = stripePos / (this.containerWidth - this.padding*2);
+    this.scrollPos            = (this.scrollWidth - this.containerWidth ) * this.pos;
 
-        ratio     = scrollWidth / containerWidth;
-        stripePos = e.pageX - padding - posFromLeft; // the mouse X position, "normalized" to the carousel position
+    // temporary add smoothness to the scroll 
+    this.DOM.wrap.style.scrollBehavior = 'smooth';
+    
+    if( this.scrollPos < 0 )
+      this.scrollPos = 0;
+    
+    if( this.scrollPos > (this.scrollWidth - this.containerWidth) )
+      this.scrollPos = this.scrollWidth - this.containerWidth
 
-        if( stripePos < 0)
-            stripePos = 0;
+    this.DOM.wrap.scrollLeft = this.scrollPos
+    this.DOM.scope.style.setProperty('--scrollWidth',  (this.containerWidth / this.scrollWidth) * 100 + '%');
+    this.DOM.scope.style.setProperty('--scrollLleft',  (this.scrollPos / this.scrollWidth ) * 100 + '%');
 
-        pos       = stripePos / (containerWidth - padding*2); // calculated position between 0 to 1
-        // calculate the percentage of the mouse position within the carousel
-        scrollPos = (scrollWidth - containerWidth ) * pos;
+    // lock UI until mouse-enter scroll is finihsed, after aprox 200ms
+    clearTimeout(this.animated)
+    this.animated = setTimeout(() => {
+      this.animated = null
+      this.DOM.wrap.style.scrollBehavior = 'auto';
+    }, 200)
 
-        el.scrollLeft = scrollPos;
+    return this
+  },
 
-        // check if element has reached an edge
-        prevMore = el.scrollLeft > 0;
-        nextMore = el.scrollLeft < (scrollWidth - containerWidth);
+  // move the stripe left or right according to mouse position
+  onMouseMove(e){
+    // don't move anything until inital movement on 'mouseenter' has finished
+    if( this.animated ) return
 
-        $el[prevMore ? "addClass" : "removeClass"]('left');
-        $el[nextMore ? "addClass" : "removeClass"]('right');
-    }
+    this.ratio = this.scrollWidth / this.containerWidth
+    
+    // the mouse X position, "normalized" to the carousel position
+    var stripePos = e.pageX - this.padding - this.posFromLeft 
+    
+    if( stripePos < 0 )
+        stripePos = 0
 
-    $.fn.carousel = function(options){
-        $(document)
-            .on('mouseenter.carousel', '.' + bindToClass, calc)
-            .on('mousemove.carousel', '.' + bindToClass, move);
-    };
+    // calculated position between 0 to 1
+    this.pos = stripePos / (this.containerWidth - this.padding*2) 
+    
+    // calculate the percentage of the mouse position within the carousel
+    this.scrollPos = (this.scrollWidth - this.containerWidth ) * this.pos 
 
-    // automatic binding to all elements which have the class that is assigned to "bindToClass"
-    $.fn.carousel();
+    this.DOM.wrap.scrollLeft = this.scrollPos
+    
+    // update scrollbar
+    if( this.scrollPos < (this.scrollWidth - this.containerWidth) )
+      this.DOM.scope.style.setProperty('--scrollLleft',  (this.scrollPos / this.scrollWidth ) * 100 + '%');
 
-})(jQuery);
+    // check if element has reached an edge
+    this.prevMore = this.DOM.wrap.scrollLeft > 0
+    this.nextMore = this.scrollWidth - this.containerWidth - this.DOM.wrap.scrollLeft > 5
+    
+    this.DOM.scope.setAttribute('data-at',
+      (this.prevMore  ? 'left ' : ' ')
+      + (this.nextMore ? 'right' : '')
+    )
+  }
+}                        
